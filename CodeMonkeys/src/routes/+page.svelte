0fff4d1252b66109c41a839eaed7a661337982c8
annaporti <a.porti@icgc.cat>
2023-11-25 16:56:44 +0100

    mapes sí, typescript no

--- CodeMonkeys/src/routes/+page.svelte
+++ CodeMonkeys/src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Code monkeys project here we gooo</h1>
-<p>Porti will take care of this</p>
+<script>
+// @ts-nocheck
+
+    import Map from "../components/map.svelte";
+    let map;
+
+</script>
+
+<Map {map}
+lat={41.7239596}
+lon={1.8081936}
+zoom={12}
+maxZoom={18}/>
